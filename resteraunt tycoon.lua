b4c4adcd911065614c36ffa9bff142a132438171
Ayisrael19 <ayisrael@liftforlifeacademy.org>
2021-03-18 16:51:19 -0500

    tycoon

--- resteraunt tycoon.lua
+++ resteraunt tycoon.lua
@@ -1,3 +1,7 @@
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/r0cp9NrM"))(); --UI Library
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local TycoonEvent = ReplicatedStorage.Events.ClientTycoonInput
+--functions--
 local function getDiner() 
 	for i,v in pairs(game.Workspace.Tycoons:GetChildren()) do
 		if v.Player.Value == game.Players.LocalPlayer then
@@ -5,18 +9,47 @@ local function getDiner()
 		end
 	end
 end
-
 local Diner = getDiner()
-for i,v in pairs(Diner.Items.OftenFiltered.Surface:GetDescendants()) do
-	if v:IsA("ObjectValue") and v.Name == "Bill" then
-			print(v)
-			local args = {
-    			[1] = Diner,
-    			[2] = {
-        			["name"] = "CollectBill",
-       		 	["model"] = v.Parent
-    			}
-			}
-			game:GetService("ReplicatedStorage").Events.ClientTycoonInput:FireServer(unpack(args))
-	end
+local function getMoney()
+    for i,v in pairs(Diner.Items.OftenFiltered.Surface:GetDescendants()) do
+	    if v:IsA("ObjectValue") and v.Name == "Bill" then
+		    	print(v)
+			    local args = {
+    			    [1] = Diner,
+    			    [2] = {
+        			    ["name"] = "CollectBill",
+       		 	    ["model"] = v.Parent
+    			    }
+			    }
+			    TycoonEvent:FireServer(unpack(args))
+	    end
+    end
 end
+
+local Tycoon = library:CreateWindow("Restaurant Tycoon 2")
+Tycoon:AddLabel("Money")
+Tycoon:AddToggle("AutoCollect Money", function(state)
+    getgenv().collect = state
+
+    while wait() do
+        if getgenv().collect then
+            getMoney()
+        end
+    end            
+end)
+
+local Misc = library:CreateWindow("--==Misc==--")
+--KeyBind
+Misc:AddKeyBind("Toggle Key",Enum.KeyCode.RightShift, function()
+    if game:GetService("CoreGui")["nigga"].Enabled then 
+        game:GetService("CoreGui")["nigga"].Enabled = false
+    else
+        game:GetService("CoreGui")["nigga"].Enabled = true
+    end
+end)
+--/ Destroy GUI \--
+Misc:AddButton("Destroy GUI",function()
+	game:GetService("CoreGui")["nigga"]:Destroy()
+end)
+local Credits = library:CreateWindow("Credits")
+Credits:AddLabel("Script Created By:\nKillua#0003")
